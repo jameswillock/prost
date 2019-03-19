a6149e9765f41df8cde36f78c5434d4fecd9c51e
James Willock <james.willock@gmail.com>
2019-03-19 19:00:39 +0000

    Constructor standings and commander

---
+++ test/mocks/constructorStandings/standings.js
@@ -0,0 +1,78 @@
+const standings = { 
+  table: [ 
+    {
+      position: 1,
+      points: 26,
+      wins: 1,
+      nationality: 'German',
+      constructor: 'Mercedes'
+    },
+    {
+      position: 2,
+      points: 22,
+      wins: 0,
+      nationality: 'Italian',
+      constructor: 'Ferrari'
+    },
+    {
+      position: 3,
+      points: 15,
+      wins: 0,
+      nationality: 'Austrian',
+      constructor: 'Red Bull'
+    },
+    {
+      position: 4,
+      points: 8,
+      wins: 0,
+      nationality: 'United States of America',
+      constructor: 'Haas F1 Team'
+    },
+    {
+      position: 5,
+      points: 6,
+      wins: 0,
+      nationality: 'France',
+      constructor: 'Renault'
+    },
+    {
+      position: 6,
+      points: 4,
+      wins: 0,
+      nationality: 'Italian',
+      constructor: 'Alfa Romeo'
+    },
+    {
+      position: 7,
+      points: 2,
+      wins: 0,
+      nationality: 'Italian',
+      constructor: 'Racing Point'
+    },
+    {
+      position: 8,
+      points: 1,
+      wins: 0,
+      nationality: 'Italian',
+      constructor: 'Toro Rosso'
+    },
+    {
+      position: 9,
+      points: 0,
+      wins: 0,
+      nationality: 'United Kingdom',
+      constructor: 'McLaren'
+    },
+    {
+      position: 10,
+      points: 0,
+      wins: 0,
+      nationality: 'United Kingdom',
+      constructor: 'Williams'
+    }
+  ],
+  season: '2019',
+  round: 1
+};
+
+module.exports = standings;
