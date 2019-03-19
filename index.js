77318c15346533a38259b367b53dd4a378bcc3ad
James Willock <james.willock@gmail.com>
2019-03-19 19:21:57 +0000

    Bump version

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const driverTable = require('./lib/driverTable');
 const program = require('commander');
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('-d, --drivers', 'Driver standings')
   .option('-c, --constructors', 'Constructor standings')
   .parse(process.argv);
